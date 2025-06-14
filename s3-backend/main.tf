c7f55d40902f75019665e0f4b6df58caadafdf40
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-06-15 02:25:44 +0500

    Remote state management s3-backend

---
+++ s3-backend/main.tf
@@ -0,0 +1,30 @@
+variable "region" {
+  description = "value of region"
+  default = "eu-north-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "demobucketheyyr3182681f55dcba1b"
+    key = "backend.tfstate"
+    region = "eu-north-1"
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "myserver" {
+    ami           = "ami-042b4708b1d05f512"
+    instance_type = "t3.nano"
+    tags = {
+     Name = "terraformbackend"
+    }
+}
