ddfc33c40d8be43554442a382c52e660a886c7f3
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-06-15 02:00:57 +0500

    Random Providers

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/aws"
       version = "~> 5.0"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "3.7.2"
+    }
   }
 }
 
@@ -11,8 +15,12 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+resource "random_id" "ran-id" {
+  byte_length = 8
+}
+
 resource "aws_s3_bucket" "demoBucket" {
-  bucket = "demobucketheyyr"
+  bucket = "demobucketheyyr${random_id.ran-id.hex}"
 }
 
 resource "aws_s3_object" "bucket_data" {
