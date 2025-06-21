f015b03adc5781604a7683c05566746270527a75
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-06-22 01:50:03 +0500

    aws-Vpc-Ec2-Nginx project

---
+++ aws-vpc-ec2-nginx/vpc.tf
@@ -0,0 +1,43 @@
+resource "aws_vpc" "ngnix_vpx" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "ngnix_vpc"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+    cidr_block = "10.0.1.0/24"
+    vpc_id = aws_vpc.ngnix_vpx.id
+    tags = {
+      Name = "private_subnet"
+    }
+}
+
+resource "aws_subnet" "public_subnet" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id = aws_vpc.ngnix_vpx.id
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+resource "aws_internet_gateway" "ngnix_igw" {
+  vpc_id = aws_vpc.ngnix_vpx.id
+  tags = {
+    Name = "ngnix_igw"
+  }
+}
+
+resource "aws_route_table" "ngnix_rt" {
+  vpc_id = aws_vpc.ngnix_vpx.id
+
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ngnix_igw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  route_table_id = aws_internet_gateway.ngnix_igw.id
+  subnet_id = aws_subnet.public_subnet.id
+}
