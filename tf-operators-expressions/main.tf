0f7a259148446046ca7e8127f7a4d212c1b135b0
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-07-05 02:44:41 +0500

    tf-operators-expressions

---
+++ tf-operators-expressions/main.tf
@@ -0,0 +1,33 @@
+terraform {}
+
+#number list
+variable "number_list" {
+    type = list(number)
+    default = [ 0 ,1,2,3,4,5 ]
+}
+
+
+#object list of person
+variable "person_list" {
+    type = list(object({
+      name = string
+      age = number
+    }))
+  default = [ {
+    name = "moiz",
+    age = 20
+  }, {
+    name = "ali",
+    age = 50
+  } ]
+}
+
+#map 
+variable "map_list" {
+  type = map(number)
+  default = {
+    "name" = 1
+    "age" = 2
+    "height" = 5
+  }
+}
