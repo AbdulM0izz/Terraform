8cdc6a3100dafbc87032b09426177151c8d8bfce
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-07-07 02:43:28 +0500

    Terraform functions

---
+++ tf-functions/main.tf
@@ -0,0 +1,18 @@
+terraform {}
+
+locals {
+  value = "Hello world"
+}
+
+variable "string_list" {
+  type = list(string)
+  default = [ "ser1", "ser2" ,"ser3" ]
+}
+
+output "name" {
+#   value = lower(local.value)
+# value = upper(local.value)
+value = startswith(local.value, "Hello")
+
+}
+
